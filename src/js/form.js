d582ef4a10619b13906b186daa9843adbac58dbb
olegmyltsev <oleg.myltsev.2008@gmail.com>
2023-04-24 23:22:41 +0300

    form finished

--- src/js/form.js
+++ src/js/form.js
@@ -13,7 +13,8 @@ form.addEventListener('submit', (event) => {
     phone: ${phone.value}, 
     email: ${email.value},
     order: ${order.value}`
-    function massage() { fetch(`https://api.telegram.org/bot6130561127:AAGc09-QEuhaIX-LFecXFsgLBWsKGf4ih08/sendMessage?chat_id=-1001937252193&parse_mode=html&text=${massage}`) }
+    fetch(`https://api.telegram.org/bot6130561127:AAGc09-QEuhaIX-LFecXFsgLBWsKGf4ih08/sendMessage?chat_id=-1001937252193&parse_mode=html&text=${message}`) 
+    console.log();
 })
 
 
